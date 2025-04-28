d9cac4dcb4773d1b37e0fdc2e354c7701987abab
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2025-04-28 21:22:45 +0300

    Remove unused code and streamline INode interface by consolidating dependencies

--- candy-crush-saga/Assets/_Scripts/GameCore/Interface/INode.cs
+++ candy-crush-saga/Assets/_Scripts/GameCore/Interface/INode.cs
@@ -1,17 +1,6 @@
-﻿using DG.Tweening;
-using GameCore.Scriptables;
-using UnityEngine;
-
-namespace Interface
+﻿namespace Interface
 {
-    public interface INode
+    public interface INode : INodeDetails, INodeBehavior, IPoolObject
     {
-        public Vector3 Position { get; }
-        public int Row { get; set; }
-        public int Column { get; set; }
-        public CandyType CandyType { get; set; }
-        public void Initialize(int row, int column);
-        public void SetGridPosition(int row, int column);
-        public Sequence Swap(INode otherNode);
     }
 }
